b2f9decbba03c498a276bcd69723ace08aefce1f
Nicolas Pellerin <nickypellerin@gmail.com>
2021-01-24 19:56:34 -0500

    Add link to course

--- components/Courses/data.ts
+++ components/Courses/data.ts
@@ -1,10 +1,10 @@
 export const projectsData = [
     {
-      title: "Learn Framer Motion",
+      title: "Learn Framer Motion - From beginner to advanced",
       image: "/learn-framer-motion.jpg",
       description:
         `Why Framer Motion? With over 300 000+ weekly downloads, it's one of the most popular React animation library. Its API design and powerful features make animating React components much easier than other popular animation libraries out there. In this course, you will learn the ins and out of this amazing library, all while building 2 awesome projects with multiple animated components.`,
-      website: "",
+      website: "https://learnframermotion.com",
       github: "",
       techStack: [
         { tech: "react", logo: "/icons/react.png", width: 60 },
