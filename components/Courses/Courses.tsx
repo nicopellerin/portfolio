3706b4df896f32e2ffbf35f4f4a2180c8f3eb55c
Nicolas Pellerin <nickypellerin@gmail.com>
2021-05-14 20:38:03 -0400

    Fix courses title margin

--- components/Courses/Courses.tsx
+++ components/Courses/Courses.tsx
@@ -61,11 +61,10 @@ const CardList = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
   grid-gap: 10rem;
-  margin-top: 8rem;
+  margin-top: 4rem;
 
   @media (max-width: 500px) {
     grid-template-columns: 1fr;
     grid-gap: 6rem;
-    margin-top: 4rem;
   }
 `
